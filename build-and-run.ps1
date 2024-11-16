2ad1398b6d577e6bf9d4fc802b3eac3495a4658a
Taylor Plewe <tplewe@outlook.com>
2024-11-16 15:07:01 -0700

    add template from psN00bsdk install instructions

---
+++ build-and-run.ps1
@@ -0,0 +1,5 @@
+cmake --preset default .
+cmake --build ./build
+
+$duckstationPath = 'C:\Users\tplew\Documents\Other\ps1\duckstation-windows-x64-release\duckstation-qt-x64-ReleaseLTCG.exe'
+& $duckstationPath build/psx_tutorial.bin
