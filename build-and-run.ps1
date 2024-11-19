a782ebaaf62b63278d34d80996d373af6ce6241a
Taylor Plewe <tplewe@outlook.com>
2024-11-19 08:10:10 -0700

    following tutorial's TIM(sprite) and controller pages

--- build-and-run.ps1
+++ build-and-run.ps1
@@ -1,5 +1,30 @@
-cmake --preset default .
-cmake --build ./build
+if ($args.Count -lt 1) {
+	Write-Output "please provide folder name"
+	return
+}
 
+$path = $args[0]
+cmake --preset default $path
+if ($LASTEXITCODE -ne 0) { return }
+cmake --build $path/build
+if ($LASTEXITCODE -ne 0) { return }
+
+$exeName
+$exeSearch = $(Get-ChildItem $path/build/*.exe)
+if ($exeSearch.Count -gt 0) {
+	$exeName = $exeSearch[0].Name
+} else {
+	$exeSearch = $(Get-ChildItem $path/build/*.bin)
+	if ($exeSearch.Count -gt 0) {
+		$exeName = $exeSearch[0].Name
+		Write-Output "executing .bin"
+	} else {
+		Write-Output "no .exe or .bin found in build/ folder!"
+		return
+	}
+}
+
+$pcsxReduxPath = 'C:\Users\tplew\Documents\Other\ps1\pcsx-redux-nightly-20568.20241112.3-x64-cli\pcsx-redux.exe'
 $duckstationPath = 'C:\Users\tplew\Documents\Other\ps1\duckstation-windows-x64-release\duckstation-qt-x64-ReleaseLTCG.exe'
-& $duckstationPath build/psx_tutorial.bin
+#& $duckstationPath $path/build/$exeName
+& $pcsxReduxPath -exe $path/build/$exeName -run
--- main.c
+++ main.c
@@ -3,10 +3,15 @@
 #include <psxetc.h>
 #include <psxgte.h>
 #include <psxgpu.h>
+#include <psxapi.h>
+#include <psxpad.h>
 #define OTLEN 8
 #define BOX_SIZE 64
 #define SCRN_WIDTH 320
 #define SCRN_HEIGHT 240
+#define BOX1_SPEED 2
+#define BOX2_SPEED 4
+#define NUM_BOXES 2
 
 DISPENV disp[2];
 DRAWENV draw[2];
@@ -16,11 +21,66 @@ u_long ot[2][OTLEN];
 char primbuff[2][32768];
 char* nextprim;
 
-int boxX, boxY;
-int boxHspeed, boxVspeed;
+typedef struct {
+	int x;
+	int y;
+	int hspeed;
+	int vspeed;
+	int width;
+	int height;
+	int r, g, b;
+} Box;
+Box* boxes[NUM_BOXES];
+char* boxbuff[sizeof(Box)*NUM_BOXES];
 
+TILE* tile;
+SPRT* sprt;
+DR_TPAGE* tpri;
+
+int sprtX = 40;
+
+TIM_IMAGE ti;
+extern const uint32_t garden64_1_tim[];
+
+u_char padbuff[2][34];
+PADTYPE padinfo;
+
+void loadTexture(const uint32_t* tim, TIM_IMAGE* tparam) {
+	GetTimInfo(tim, tparam);
+	LoadImage(tparam->prect, tparam->paddr);
+	DrawSync(0);
+	if (tparam->mode & 0x8) {
+		LoadImage(tparam->crect, tparam->caddr);
+		DrawSync(0);
+	}
+}
+void initBoxes() {
+	boxes[0] = (Box*)boxbuff;
+	boxes[1] = (Box*)boxbuff+sizeof(Box);
+
+	boxes[0]->x = 32;
+	boxes[0]->y = 32;
+	boxes[0]->hspeed = BOX1_SPEED;
+	boxes[0]->vspeed = BOX1_SPEED;
+	boxes[0]->width = BOX_SIZE;
+	boxes[0]->height = BOX_SIZE;
+	boxes[0]->r = 240;
+	boxes[0]->g = 240;
+	boxes[0]->b = 120;
+
+	boxes[1]->x = 98;
+	boxes[1]->y = 120;
+	boxes[1]->hspeed = BOX1_SPEED;
+	boxes[1]->vspeed = BOX1_SPEED;
+	boxes[1]->width = BOX_SIZE;
+	boxes[1]->height = BOX_SIZE;
+	boxes[1]->r = 120;
+	boxes[1]->g = 240;
+	boxes[1]->b = 120;
+}
 void init() {
 	ResetGraph(0);
+	loadTexture(garden64_1_tim, &ti);
 
 	SetDefDispEnv(&disp[0], 0, 0, SCRN_WIDTH, SCRN_HEIGHT);
 	SetDefDispEnv(&disp[1], 0, SCRN_HEIGHT, SCRN_WIDTH, SCRN_HEIGHT);
@@ -31,10 +91,17 @@ void init() {
 	setRGB0(&draw[1], 50, 200, 50);
 	draw[0].isbg = 1;
 	draw[1].isbg = 1;
+	// draw[0].tpage = getTPage(ti.mode&0x3, 0, ti.prect->x, ti.prect->y);
+	// draw[1].tpage = getTPage(ti.mode&0x3, 0, ti.prect->x, ti.prect->y);
 
 	PutDispEnv(&disp[0]);
 	SetDispMask(1);
 	PutDrawEnv(&draw[0]);
+
+	InitPAD(padbuff[0], 34, padbuff[1], 34);
+	padbuff[0][0] = padbuff[0][1] = 0xff;
+	padbuff[1][0] = padbuff[1][1] = 0xff;
+	StartPAD();
 
 	FntLoad(960, 0);
 	FntOpen(0, 8, 320, 224, 0, 100);
@@ -42,12 +109,24 @@ void init() {
 	nextprim = primbuff[0];
 
 	db = 0;
+	
+	initBoxes();
 }
 
+void getInput() {
+	padinfo = *((PADTYPE*)padbuff[0]);
+
+	if (padinfo.stat) {
+		return;
+	}
+
+	if (!(padinfo.btn & PAD_CROSS)) {
+		sprtX++;
+	}
+}
 void display() {
 	DrawSync(0);
 	VSync(0);
-
 	
 	PutDispEnv(&disp[db]);
 	PutDrawEnv(&draw[db]);
@@ -58,49 +137,63 @@ void display() {
 	nextprim = primbuff[db];
 }
 
-void updateBox() {
-	boxX += boxHspeed;
-	boxY += boxVspeed;
+void updateBox(Box* box) {
+	box->x += box->hspeed;
+	box->y += box->vspeed;
 
-	if (boxX + BOX_SIZE > SCRN_WIDTH) {
-		boxX = SCRN_WIDTH - BOX_SIZE;
-		boxHspeed *= -1;
-	} else if (boxX < 0) {
-		boxX = 0;
-		boxHspeed *= -1;
+	if (box->x + BOX_SIZE > SCRN_WIDTH) {
+		box->x = SCRN_WIDTH - BOX_SIZE;
+		box->hspeed *= -1;
+	} else if (box->x < 0) {
+		box->x = 0;
+		box->hspeed *= -1;
 	}
-	if (boxY + BOX_SIZE > SCRN_HEIGHT) {
-		boxY = SCRN_HEIGHT - BOX_SIZE;
-		boxVspeed *= -1;
-	} else if (boxY < 0) {
-		boxY = 0;
-		boxVspeed *= -1;
+	if (box->y + BOX_SIZE > SCRN_HEIGHT) {
+		box->y = SCRN_HEIGHT - BOX_SIZE;
+		box->vspeed *= -1;
+	} else if (box->y < 0) {
+		box->y = 0;
+		box->vspeed *= -1;
 	}
+	
+	tile = (TILE*)nextprim;
+	setTile(tile);
+	setXY0(tile, box->x, box->y);
+	setWH(tile, BOX_SIZE, BOX_SIZE);
+	setRGB0(tile, box->r, box->g, box->b);
+	addPrim(ot[db], tile);
+	nextprim += sizeof(TILE);
 }
 
 int main() {
 	init();
 
-	TILE* tile;
-	boxX = 32;
-	boxY = 32;
-	boxHspeed = 1;
-	boxVspeed = 1;
-
 	while (1) {
+		getInput();
 		FntPrint(0, "Hello California");
 		FntFlush(-1);
 
-		updateBox();
-
 		ClearOTagR(ot[db], OTLEN);
-		tile = (TILE*)nextprim;
-		setTile(tile);
-		setXY0(tile, boxX, boxY);
-		setWH(tile, BOX_SIZE, BOX_SIZE);
-		setRGB0(tile, 255, 255, 0);
-		nextprim += sizeof(TILE);
-		addPrim(ot[db], tile);
+
+		for (int i = 0; i < NUM_BOXES; i++) {
+			updateBox(boxes[i]);
+		}
+
+		sprt = (SPRT*)nextprim;
+		setSprt(sprt);
+		setXY0(sprt, sprtX, 40);
+		setUV0(sprt, 0, 0);
+		setWH(sprt, 64, 64);
+		setClut(sprt, ti.crect->x, ti.crect->y);
+		setRGB0(sprt, 128, 128, 128);
+		addPrim(ot[db], sprt);
+		nextprim += sizeof(SPRT);
+
+		tpri = (DR_TPAGE*)nextprim;
+		setDrawTPage( tpri, 0, 1, 
+			getTPage(ti.mode&0x3, 0, ti.prect->x, ti.prect->y));
+		addPrim( ot[db], tpri );
+		nextprim += sizeof(DR_TPAGE);
 
 		display();
 	}
